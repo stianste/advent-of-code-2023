70cecb5e7bb3455ea8f3cc7a4d48be7d90711e06
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-07 17:43:47 +0100

    feat: add main logic for day 4

--- day-04-scratchcards/app/Main.hs
+++ day-04-scratchcards/app/Main.hs
@@ -1,10 +1,29 @@
 module Main where
 import Data.List.Split
+import Data.List (intersect)
 
 exampleLine :: String
 exampleLine = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
 
+calculatePoints :: Int -> Int
+calculatePoints 0 = 0
+calculatePoints 1 = 1
+calculatePoints n = n * 2
+
+
 main :: IO ()
 main = do 
-  let (gameId, numbers) = break (== ':') exampleLine
-  print (gameId, tail numbers)
+  print exampleLine
+
+  let (gameId, prefixedNumbersString) = break (== ':') exampleLine
+  print (gameId, tail prefixedNumbersString)
+
+  let (winningNumbersString, numbersString) = break (== '|') (tail prefixedNumbersString)
+  print (winningNumbersString, numbersString)
+
+  let winningNumbers = map read (words winningNumbersString) :: [Int]
+  let numbers = map read (words (tail numbersString)) :: [Int]
+  print (winningNumbers, numbers)
+
+  let numOverlapping = length (winningNumbers `intersect` numbers)
+  print numOverlapping

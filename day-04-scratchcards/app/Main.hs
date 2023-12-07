13facd42647ed7a21d123e4daf706cb35bfa7ce8
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-07 20:15:01 +0100

    refactor: day 04

--- day-04-scratchcards/app/Main.hs
+++ day-04-scratchcards/app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
 import Data.List.Split
 import Data.List (intersect)
-
-exampleLine :: String
-exampleLine = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+import Data.Functor ((<&>))
+import Data.Function ((&))
 
 calculatePoints :: Int -> Int
 calculatePoints 0 = 0
@@ -11,20 +10,17 @@ calculatePoints 1 = 1
 calculatePoints n = 2 ^ (n - 1)
 
 getPointsFromLine :: String -> Int
-getPointsFromLine exampleLine = let 
-    (gameId, prefixedNumbersString) = break (== ':') exampleLine
+getPointsFromLine line = let
+    (_, prefixedNumbersString) = break (== ':') line
     (winningNumbersString, numbersString) = break (== '|') (tail prefixedNumbersString)
-    winningNumbers = map read (words winningNumbersString) :: [Int]
-    numbers = map read (words (tail numbersString)) :: [Int]
-    numOverlapping = length (winningNumbers `intersect` numbers)
+    winningNumbers = winningNumbersString & words & map read :: [Int]
+    numbers = numbersString & tail & words & map read
+    intersection = winningNumbers `intersect` numbers
   in
-    calculatePoints numOverlapping
+    (calculatePoints . length) intersection
 
 
 main :: IO ()
-main = do 
+main = do
   contents <- readFile "./app/inputs/day-04.txt"
-  let inputs = lines contents
-  let points = map getPointsFromLine inputs
-  let result = sum points
-  print result
+  print $ contents & lines & map getPointsFromLine & sum

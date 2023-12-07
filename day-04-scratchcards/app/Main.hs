ac2e0a0f3f9cdbea4e722b113e3708e48cdb7497
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-07 17:28:01 +0100

    feat: split line

--- day-04-scratchcards/app/Main.hs
+++ day-04-scratchcards/app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
+import Data.List.Split
 
 exampleLine :: String
 exampleLine = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
 
 main :: IO ()
 main = do 
-  let splitted = words exampleLine
-  print splitted
+  let (gameId, numbers) = break (== ':') exampleLine
+  print (gameId, tail numbers)

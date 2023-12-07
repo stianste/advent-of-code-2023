ac75925851790928fa396824543dace047dba866
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-07 18:09:41 +0100

    feat: add solution to first part of day 4

--- day-04-scratchcards/app/Main.hs
+++ day-04-scratchcards/app/Main.hs
@@ -8,22 +8,23 @@ exampleLine = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
 calculatePoints :: Int -> Int
 calculatePoints 0 = 0
 calculatePoints 1 = 1
-calculatePoints n = n * 2
+calculatePoints n = 2 ^ (n - 1)
+
+getPointsFromLine :: String -> Int
+getPointsFromLine exampleLine = let 
+    (gameId, prefixedNumbersString) = break (== ':') exampleLine
+    (winningNumbersString, numbersString) = break (== '|') (tail prefixedNumbersString)
+    winningNumbers = map read (words winningNumbersString) :: [Int]
+    numbers = map read (words (tail numbersString)) :: [Int]
+    numOverlapping = length (winningNumbers `intersect` numbers)
+  in
+    calculatePoints numOverlapping
 
 
 main :: IO ()
 main = do 
-  print exampleLine
-
-  let (gameId, prefixedNumbersString) = break (== ':') exampleLine
-  print (gameId, tail prefixedNumbersString)
-
-  let (winningNumbersString, numbersString) = break (== '|') (tail prefixedNumbersString)
-  print (winningNumbersString, numbersString)
-
-  let winningNumbers = map read (words winningNumbersString) :: [Int]
-  let numbers = map read (words (tail numbersString)) :: [Int]
-  print (winningNumbers, numbers)
-
-  let numOverlapping = length (winningNumbers `intersect` numbers)
-  print numOverlapping
+  contents <- readFile "./app/inputs/day-04.txt"
+  let inputs = lines contents
+  let points = map getPointsFromLine inputs
+  let result = sum points
+  print result

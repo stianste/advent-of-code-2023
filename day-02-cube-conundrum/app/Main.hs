9e2156b5ecea6f551a3723edf73e3243bd954d88
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-09 19:44:43 +0100

    day 2: read the colors

--- day-02-cube-conundrum/app/Main.hs
+++ day-02-cube-conundrum/app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import qualified Data.Map as Map  
+import Data.List.Split
+
+type ColorMap = Map.Map String Int
+colorMap :: ColorMap
+colorMap = Map.fromList [("red", 12), ("green", 13), ("blue", 14)]
+
+replaceSemicolon :: String -> String  
+replaceSemicolon = map (\c -> if c == ';' then ',' else c)  
+
+-- Example: "Game 2:1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue" -> "1 blue, 2 green, 3 green, 4 blue, 1 red, 1 green, 1 blue"
+
+getRevealTuples :: String -> [(String, Int)]
+getRevealTuples s = let
+    (gameNumber, reveals) = break (== ':') s
+    sanitisedReveals = replaceSemicolon (tail reveals)
+    revealList = splitOn ", " sanitisedReveals
+    tupleList = map (break (== ' ')) revealList
+  in
+    [(tail (snd x), read (fst x) :: Int) | x <- tupleList ] 
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  print $ getRevealTuples "Game 1:3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"

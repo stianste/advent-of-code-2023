51bd38271ef1967626d3c79de815d35fbf7de7b1
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-09 16:07:56 +0100

    day 06: initial solution

--- day-06-wait-for-it/app/Main.hs
+++ day-06-wait-for-it/app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+calculateLength :: Int -> Int -> Int
+calculateLength raceDuration candidateHoldTime = candidateHoldTime * (raceDuration - candidateHoldTime)
+
+processRace :: (Int, Int) -> Int
+processRace race = let
+    (duration, record) = race
+    candidates = [1..(fst race - 1)]
+  in
+    length $ filter (> record) (map (calculateLength duration) candidates)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  -- let races = [(7, 9), (15, 40), (30, 200)]
+  let races = [(59, 543), (68, 1020), (82, 1664), (74, 1022)] 
+  let result = product $ map processRace races
+  print result

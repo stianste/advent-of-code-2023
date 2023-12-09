9d934d1dcb1c1c762e8ce3f9f78b568ac64fd328
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-09 19:53:02 +0100

    day 2: check if any game is invalid

--- day-02-cube-conundrum/app/Main.hs
+++ day-02-cube-conundrum/app/Main.hs
@@ -1,14 +1,14 @@
 module Main where
 
-import qualified Data.Map as Map  
+import qualified Data.Map as Map
 import Data.List.Split
 
 type ColorMap = Map.Map String Int
 colorMap :: ColorMap
 colorMap = Map.fromList [("red", 12), ("green", 13), ("blue", 14)]
 
-replaceSemicolon :: String -> String  
-replaceSemicolon = map (\c -> if c == ';' then ',' else c)  
+replaceSemicolon :: String -> String
+replaceSemicolon = map (\c -> if c == ';' then ',' else c)
 
 -- Example: "Game 2:1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue" -> "1 blue, 2 green, 3 green, 4 blue, 1 red, 1 green, 1 blue"
 
@@ -19,9 +19,19 @@ getRevealTuples s = let
     revealList = splitOn ", " sanitisedReveals
     tupleList = map (break (== ' ')) revealList
   in
-    [(tail (snd x), read (fst x) :: Int) | x <- tupleList ] 
+    [(tail (snd x), read (fst x) :: Int) | x <- tupleList ]
+
+tupleIsInvalid  :: ColorMap -> (String, Int) -> Bool
+tupleIsInvalid colorMap (color, presentedValue) = case Map.lookup color colorMap of
+  Just maxValue ->  presentedValue > maxValue
+  Nothing -> False
+
+anyViolations :: ColorMap -> [(String, Int)] -> Bool
+anyViolations colorMap = any (tupleIsInvalid colorMap)
 
 
 main :: IO ()
 main = do
-  print $ getRevealTuples "Game 1:3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+  let tuples = getRevealTuples "Game 1:3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+  print tuples
+  print $ anyViolations colorMap tuples

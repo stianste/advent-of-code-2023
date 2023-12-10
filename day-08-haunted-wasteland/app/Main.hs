f111a464c5edb90bd7c19075a57fa51ce79ae3eb
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-10 11:19:36 +0100

    day 08: use least common multiple to solve part 2
    My naive solution did not work. Thank you reddit :shrug:

--- day-08-haunted-wasteland/app/Main.hs
+++ day-08-haunted-wasteland/app/Main.hs
@@ -15,7 +15,7 @@ parseLine line = let
 
 getNextInstruction :: (String, Int) -> Direction
 getNextInstruction (instructions, index)
-  | instructions !! (index `mod` length instructions) == 'R' = RIGHT 
+  | instructions !! (index `mod` length instructions) == 'R' = RIGHT
   | otherwise = LEFT
 
 traverseWithInstructions :: (String, NodeMap, String, Int) -> Int
@@ -28,32 +28,35 @@ traverseWithInstructions (key, nodeMap, instructions, depth) = let
   in
     traverseWithInstructions (nextKey, nodeMap, instructions, depth + 1)
 
-traverseWithInstructionsMultiple :: ([String], NodeMap, String, Int) -> Int
-traverseWithInstructionsMultiple (keys, nodeMap, instructions, depth) = let
-    nextDirections = map(\key -> fromJust (Map.lookup key nodeMap)) keys
+traverseWithInstructionsEndsWithZ :: (String, NodeMap, String, Int) -> Int
+traverseWithInstructionsEndsWithZ  (key, nodeMap, instructions, depth) = let
+    nextDirections = fromJust (Map.lookup key nodeMap)
     nextInstruction = getNextInstruction (instructions, depth)
-    nextKeys = map (if nextInstruction == RIGHT then snd else fst ) nextDirections
+    nextKey = if nextInstruction == RIGHT then snd nextDirections else fst nextDirections
   in
-    if all (elem 'Z') keys then depth else traverseWithInstructionsMultiple(nextKeys, nodeMap, instructions, depth + 1)
+    if last key == 'Z' then depth else traverseWithInstructionsEndsWithZ (nextKey, nodeMap, instructions, depth + 1)
 
+lcmList :: [Int] -> Int
+lcmList = foldl1 lcm
 
 main :: IO ()
 main = do
-  contents <- readFile "./app/inputs/day-08-test-2.txt"
+  contents <- readFile "./app/inputs/day-08.txt"
   print $ getNextInstruction ("RLLLR", 0) == RIGHT
   print $ getNextInstruction ("RLLLR", 1) == LEFT
   print $ getNextInstruction ("RLLLR", 4) == RIGHT
   print $ getNextInstruction ("RLLLR", 5) == RIGHT
   print $ getNextInstruction ("RLLLR", 6) == LEFT
 
-  let instructions = "LR"
-  -- let instructions = "LLRLRRRLLLRLRRLRRRLRLRRLRLRLRRRLRRRLRLRLRRLLRRRLRRLRRLLRLRRRLRLRLLRRRLLRRRLRLRRRLRRRLRRRLLLRRRLRRLRRLRLRRLRLRRRLRLRRLRLRLRRRLRLLLRRRLLLRLRRRLRLRRLRLRLRLRRLRRLRRLRLRRRLRRRLRRLRRRLRRLRRLRRRLLRLRRLLLRRLRRLRLRLLLRRLRRLRRRLRRLLRLRRRLRRRLRRLRRLRLRRLRLRRRLRRLRRRLLRRRLRLRLLLRRRLLLRRLLRRLRLRRLRLLLRRRR"
+  -- let instructions = "LR"
+  let instructions = "LLRLRRRLLLRLRRLRRRLRLRRLRLRLRRRLRRRLRLRLRRLLRRRLRRLRRLLRLRRRLRLRLLRRRLLRRRLRLRRRLRRRLRRRLLLRRRLRRLRRLRLRRLRLRRRLRLRRLRLRLRRRLRLLLRRRLLLRLRRRLRLRRLRLRLRLRRLRRLRRLRLRRRLRRRLRRLRRRLRRLRRLRRRLLRLRRLLLRRLRRLRLRLLLRRLRRLRRRLRRLLRLRRRLRRRLRRLRRLRLRRLRLRRRLRRLRRRLLRRRLRLRLLLRRRLLLRRLLRRLRLRRLRLLLRRRR"
 
   let nodeMap = Map.fromList (map parseLine (lines contents))
   -- let result = traverseWithInstructions ("AAA", nodeMap, instructions, 0)
-  let startingNodes = filter (elem 'A') (Map.keys nodeMap)
+
+  let startingNodes = filter (\key -> last key == 'A') (Map.keys nodeMap)
   print startingNodes
+  let solutions = map (\key -> traverseWithInstructionsEndsWithZ (key, nodeMap, instructions, 0)) startingNodes
+  let solutionSync = lcmList solutions
 
-  let result = traverseWithInstructionsMultiple (startingNodes, nodeMap, instructions, 0)
-
-  print result
+  print solutionSync

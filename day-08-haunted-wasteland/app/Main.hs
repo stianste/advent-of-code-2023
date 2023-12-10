6769310acae1ffa53e55ce8098e13a284d6c8796
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-10 10:19:43 +0100

    day 8: pass test case for part 2

--- day-08-haunted-wasteland/app/Main.hs
+++ day-08-haunted-wasteland/app/Main.hs
@@ -4,7 +4,7 @@ import Data.Maybe (fromJust)
 
 data Direction = LEFT | RIGHT deriving (Eq, Show)
 
-type InstructionsMap = Map.Map String (String, String)
+type NodeMap = Map.Map String (String, String)
 
 parseLine :: String -> (String, (String, String))
 parseLine line = let
@@ -18,29 +18,42 @@ getNextInstruction (instructions, index)
   | instructions !! (index `mod` length instructions) == 'R' = RIGHT 
   | otherwise = LEFT
 
-traverseWithInstructions :: (String, InstructionsMap, String, Int) -> Int
+traverseWithInstructions :: (String, NodeMap, String, Int) -> Int
 traverseWithInstructions  ("ZZZ", _, _, depth) = depth
 
-traverseWithInstructions (key, instructionsMap, instructions, depth) = let
-    nextDirections = fromJust (Map.lookup key instructionsMap)
+traverseWithInstructions (key, nodeMap, instructions, depth) = let
+    nextDirections = fromJust (Map.lookup key nodeMap)
     nextInstruction = getNextInstruction (instructions, depth)
     nextKey = if nextInstruction == RIGHT then snd nextDirections else fst nextDirections
   in
-    traverseWithInstructions (nextKey, instructionsMap, instructions, depth + 1)
+    traverseWithInstructions (nextKey, nodeMap, instructions, depth + 1)
+
+traverseWithInstructionsMultiple :: ([String], NodeMap, String, Int) -> Int
+traverseWithInstructionsMultiple (keys, nodeMap, instructions, depth) = let
+    nextDirections = map(\key -> fromJust (Map.lookup key nodeMap)) keys
+    nextInstruction = getNextInstruction (instructions, depth)
+    nextKeys = map (if nextInstruction == RIGHT then snd else fst ) nextDirections
+  in
+    if all (elem 'Z') keys then depth else traverseWithInstructionsMultiple(nextKeys, nodeMap, instructions, depth + 1)
+
 
 main :: IO ()
 main = do
-  contents <- readFile "./app/inputs/day-08.txt"
-  print $ head (map parseLine (lines contents))
+  contents <- readFile "./app/inputs/day-08-test-2.txt"
   print $ getNextInstruction ("RLLLR", 0) == RIGHT
   print $ getNextInstruction ("RLLLR", 1) == LEFT
   print $ getNextInstruction ("RLLLR", 4) == RIGHT
   print $ getNextInstruction ("RLLLR", 5) == RIGHT
   print $ getNextInstruction ("RLLLR", 6) == LEFT
 
-  -- let instructions = "LLR"
-  let instructions = "LLRLRRRLLLRLRRLRRRLRLRRLRLRLRRRLRRRLRLRLRRLLRRRLRRLRRLLRLRRRLRLRLLRRRLLRRRLRLRRRLRRRLRRRLLLRRRLRRLRRLRLRRLRLRRRLRLRRLRLRLRRRLRLLLRRRLLLRLRRRLRLRRLRLRLRLRRLRRLRRLRLRRRLRRRLRRLRRRLRRLRRLRRRLLRLRRLLLRRLRRLRLRLLLRRLRRLRRRLRRLLRLRRRLRRRLRRLRRLRLRRLRLRRRLRRLRRRLLRRRLRLRLLLRRRLLLRRLLRRLRLRRLRLLLRRRR"
+  let instructions = "LR"
+  -- let instructions = "LLRLRRRLLLRLRRLRRRLRLRRLRLRLRRRLRRRLRLRLRRLLRRRLRRLRRLLRLRRRLRLRLLRRRLLRRRLRLRRRLRRRLRRRLLLRRRLRRLRRLRLRRLRLRRRLRLRRLRLRLRRRLRLLLRRRLLLRLRRRLRLRRLRLRLRLRRLRRLRRLRLRRRLRRRLRRLRRRLRRLRRLRRRLLRLRRLLLRRLRRLRLRLLLRRLRRLRRRLRRLLRLRRRLRRRLRRLRRLRLRRLRLRRRLRRLRRRLLRRRLRLRLLLRRRLLLRRLLRRLRLRRLRLLLRRRR"
 
-  let instructionsMap = Map.fromList (map parseLine (lines contents))
-  let result = traverseWithInstructions ("AAA", instructionsMap, instructions, 0)
+  let nodeMap = Map.fromList (map parseLine (lines contents))
+  -- let result = traverseWithInstructions ("AAA", nodeMap, instructions, 0)
+  let startingNodes = filter (elem 'A') (Map.keys nodeMap)
+  print startingNodes
+
+  let result = traverseWithInstructionsMultiple (startingNodes, nodeMap, instructions, 0)
+
   print result

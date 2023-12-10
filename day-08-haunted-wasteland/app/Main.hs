250f4a36b01e3f5f4101c11d60f2578cc7e92af2
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-10 09:03:14 +0100

    day 08: parse a single line

--- day-08-haunted-wasteland/app/Main.hs
+++ day-08-haunted-wasteland/app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
+import qualified Data.Map as Map
+
+--RL
+
+-- AAA=("BBB", "CCC")
+-- BBB=("DDD", "EEE")
+-- CCC=("ZZZ", "GGG")
+-- DDD=("DDD", "DDD")
+-- EEE=("EEE", "EEE")
+-- GGG=("GGG", "GGG")
+-- ZZZ=("ZZZ", "ZZZ")
+
+type InstructionsMap = Map.Map String (String, String)
+
+parseLine :: String -> (String, (String, String))
+parseLine line = let
+    (key, instructions) = break (== '=') line
+    parsedInstructions = read (tail instructions) :: (String, String)
+  in
+    (key, parsedInstructions)
+
+-- traverse :: (IntrsuctionsMap, String, Int) -> Int
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  contents <- readFile "./app/inputs/day-08-test.txt"
+  print $ head (map parseLine (lines contents))

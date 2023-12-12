2f2b96e06be68a78e152224c01439a811962c890
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-12 21:29:28 +0100

    day 9: solve part 1

--- day-09-mirage-maintenance/app/Main.hs
+++ day-09-mirage-maintenance/app/Main.hs
@@ -14,7 +14,7 @@ predictNextValue :: [Int] -> Int -> Int
 predictNextValue values cumSum = let
     differences = getDifferences values
   in
-    if all (== last values) differences then cumSum else predictNextValue differences (last values + cumSum)
+    if all (== 0) differences then last values + cumSum else predictNextValue differences (last values + cumSum)
 
 
 main :: IO ()
@@ -22,6 +22,10 @@ main = do
   let line = parseLine "0 3 6 9 12 15"
   print line
   print $ getDifferences [0, 3, 6, 9, 12, 15] == [3, 3, 3, 3, 3]
-  print $ predictNextValue [0, 3, 6, 9, 12, 15] 0
-  print $ predictNextValue [1, 3, 6, 10, 15, 21] 0
-  print $ predictNextValue [10, 13, 16, 21, 30, 45] 0
+  print $ predictNextValue [0, 3, 6, 9, 12, 15] 0 == 18
+  print $ predictNextValue [1, 3, 6, 10, 15, 21] 0 == 28
+  print $ predictNextValue [10, 13, 16, 21, 30, 45] 0 == 68
+  content <- readFile "./app/inputs/day-09.txt"
+  let result = map (\line -> predictNextValue (parseLine line) 0) (lines content)
+  print result
+  print $ sum result

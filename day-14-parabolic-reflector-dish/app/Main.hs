d26f1b635475519aa9412492f9f52f94def865dd
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-18 19:45:25 +0100

    feat: add (misunderstood) solution to day 14

--- day-14-parabolic-reflector-dish/app/Main.hs
+++ day-14-parabolic-reflector-dish/app/Main.hs
@@ -1,4 +1,42 @@
 module Main where
+import Data.List (transpose)
+
+exampleInput :: String
+exampleInput = "O....#...." ++
+               "O.OO#....#" ++
+               ".....##..." ++
+               "OO.#O....O" ++
+               ".O.....O#." ++
+               "O.#..O.#.#" ++
+               "..O..#O..O" ++
+               ".......O.." ++
+               "#....###.." ++
+               "#OO..#...."
+
+partialTriangleFunction :: Int -> Int -> Int
+partialTriangleFunction n numberToTake = sum $ take numberToTake [n,(n-1)..]
+
+calculateColumnValue :: String -> Int -> Int
+calculateColumnValue input startValue = let
+    numToTake = length $ filter (/='.') (takeWhile (/='#') input)
+  in
+    partialTriangleFunction startValue numToTake
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let test = partialTriangleFunction 10 4
+  print test
+  let testColumn = "0000..#"
+  let columnResult = calculateColumnValue testColumn 10
+
+  content <- readFile "./app/inputs/day-14-test.txt"
+  let columns = transpose $ lines content
+  let m = length $ head columns
+  print m
+  print columns
+  let results = map (`calculateColumnValue` m) columns
+  print results
+  let result = sum results
+  print result
+

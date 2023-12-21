7563d21904f95e52fd317d7186825dcdcc82e374
Stian Steinbakken <stian.steinbakken@posten.no>
2023-12-21 16:49:21 +0100

    day 15: add hash function

--- day-15-lens-library/app/Main.hs
+++ day-15-lens-library/app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
+import Data.Function ((&))
+
+
+import Data.Char
+
+hashAlgorithm :: Int -> Int -> [Char] -> Int
+hashAlgorithm currentSum index target
+  | index == length target = currentSum
+  | otherwise = hashAlgorithm (target !! index & ord & (+) currentSum & (*) 17 & (`mod` 256)) (index + 1) target
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  print $ hashAlgorithm 0 0 "HASH"
